5905086a2c8d15dfaa7698c29c5d362f1a4fd008
KimJaeHyeong <35924958+cogito1016@users.noreply.github.com>
2024-02-02 17:05:12 +0900

    Time: 74 ms (20.36%), Space: 52.1 MB (6.65%) - LeetHub

--- 2723-add-two-promises/2723-add-two-promises.ts
+++ 2723-add-two-promises/2723-add-two-promises.ts
@@ -1,12 +1,8 @@
 type P = Promise<number>
 
 async function addTwoPromises(promise1: P, promise2: P): P {
-    const p1 = await promise1.then()
-    const p2 = await promise2.then()
-    
-    return new Promise(
-        resolve=>resolve(p1+p2)
-    )
+    const [v1,v2] = await Promise.all([promise1,promise2]);
+    return v1+v2;
 };
 
 /**
