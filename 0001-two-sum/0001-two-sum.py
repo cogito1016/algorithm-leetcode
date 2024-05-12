c10ef965ad0f4c3e58e58bba8591e6b36cf06f47
KimJaeHyeong <35924958+cogito1016@users.noreply.github.com>
2024-05-12 18:24:58 +0900

    Time: 2359 ms (20.75%), Space: 12.4 MB (77.37%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -5,14 +5,10 @@ class Solution(object):
         
         for i in range(0, len(nums)):
             for j in range(i+1,len(nums)):
-                if(nums[i]+nums[j] != target):
-                    continue
                 if(nums[i]+nums[j] == target):
                     result_idx_1 = i
                     result_idx_2 = j
                     break
-            if(result_idx_1+result_idx_2 == target):
-                break
         
         return [result_idx_1,result_idx_2]
         
