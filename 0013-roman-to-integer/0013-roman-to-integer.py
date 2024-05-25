dd9bf97295c73d686b6e1c7774d42d7d3370574d
KimJaeHyeong <35924958+cogito1016@users.noreply.github.com>
2024-05-25 15:22:42 +0900

    Time: 40 ms (15.40%), Space: 11.7 MB (23.65%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,62 +1,29 @@
 class Solution(object):
-    def changeException(self, s):
-        if s == 'IV':
-            return 4
-        elif s== 'IX':
-            return 9;
-        elif s== 'XL':
-            return 40;
-        elif s== 'XC':
-            return 90;
-        elif s== 'CD':
-            return 400;
-        elif s== 'CM':
-            return 900;
-        else:
-            return 0;
-        
     def romanToInt(self, s):
         """
         :type s: str
         :rtype: int
         """
-        result = self.changeException(s)
-        if result != 0:
-            return result
-
-        sum = 0;
-
-        i = 0
-        while i < len(s):
-            gap = len(s) - i -1
-            
-            
-            if gap != 0:
-                c = s[i]+s[i+1]
-                t = self.changeException(c)
-                if t!=0:
-                    sum+=t
-                    i+=2
-                    continue
-            
-            c = s[i]
-            
-            if c == 'I':
-                sum += 1;
-            elif c == 'V':
-                sum += 5;
-            elif c == 'X':
-                sum += 10;
-            elif c == 'L':
-                sum += 50;
-            elif c == 'C':
-                sum += 100;
-            elif c == 'D':
-                sum += 500;
-            elif c == 'M':
-                sum += 1000;
+        map = {
+            "I":1,
+            "V":5,
+            "X":10,
+            "L":50,
+            "C":100,
+            "D":500,
+            "M":1000
+        }
+        
+        sum = 0
+        
+        for i in range(len(s)):
+            if (i < len(s)-1 ) and (map.get(s[i]) <map.get(s[i+1])):
+                sum -= map.get(s[i])
+            else:
+                sum += map.get(s[i])
                 
-            i+=1
-
-        return sum;
+        return sum
+                
+        
+        
         
