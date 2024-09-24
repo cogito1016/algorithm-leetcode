c6b314a29e13a87d74d0b6c573de133e83808fcd
김재형 <cogito1016@naver.com>
2024-09-24 19:40:00 +0900

    refactor: 배열스택, 클래스화 그리고 self의 사용, 인스턴스화

--- backjoon/stack/array_stack_class.py
+++ backjoon/stack/array_stack_class.py
@@ -2,5 +2,47 @@ class ArrayStack:
     #self는 무조건 첫 번째 매개변수이다. (객체 자신을 의미한다)
     def __init__(self, capacity): #파이썬의 멤버필드는 생성자에서 선언하고초기화한다.
         self.capacity = capacity
-        self.array = [None]*capacity
-        self.top = -1
+        self.arr = [None]*capacity
+        self.pointer = -1
+
+
+    def add(self, n):
+        self.pointer += 1
+        self.arr[self.pointer] = n
+
+    def isEmpty(self):
+        if self.arr[0] == None:
+            return True
+        return False
+
+    def pop(self):
+        if (self.isEmpty()):
+            return None
+        result = self.arr[self.pointer]
+        self.arr[self.pointer] = None
+        self.pointer -= 1
+        return result
+
+    def peek(self):
+        if (self.isEmpty()):
+            return None
+        return self.arr[self.pointer]
+
+stack = ArrayStack(10)
+
+
+print(stack.arr) #[None, None, None, None, None, None, None, None, None, None]
+stack.add(3)
+print(stack.arr) #[3, None, None, None, None, None, None, None, None, None]
+stack.add(1)
+print(stack.arr) #[3, 1, None, None, None, None, None, None, None, None]
+stack.add(2)
+print(stack.arr) #[3, 1, 2, None, None, None, None, None, None, None]
+print(stack.isEmpty()) #False
+print(stack.pop()) #2
+print(stack.pop()) #1
+print(stack.pop()) #3
+print(stack.isEmpty()) #True
+stack.add(8)
+print(stack.arr) #[8, None, None, None, None, None, None, None, None, None]
+print(stack.peek()) #8
