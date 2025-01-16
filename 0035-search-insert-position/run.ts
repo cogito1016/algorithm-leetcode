4b6d9996f4e2a896dc841bf2d47f27e284bd9a5c
김재형 <cogito1016@naver.com>
2025-01-16 20:16:34 +0900

    refactor: 이분탐색 개선, 적절한위치찾는 로직은 굳이 추가할필요가 없다

--- 0035-search-insert-position/run.ts
+++ 0035-search-insert-position/run.ts
@@ -1,28 +1,23 @@
 const nums:number[] = [1,3,5,7];
-const target:number = 4;
+const target:number = 6;
 console.log(searchInsert(nums,target))
 
 function searchInsert(nums: number[], target: number): number {
     let left:number,right:number,mid:number;
     left = 0;
     right = nums.length;
-    mid = Math.floor((left+right)/2)
 
-    while(nums[mid]!==target){
-        let midValue = nums[mid];
-        if(midValue>=target){
-            right = mid
-        }else{
-            left = mid
-        }
-        mid = Math.floor((left+right)/2)
-        if(midValue===nums[mid]){
-            if(target>nums[mid]){
-                return mid+1
-            }
+    while(left<right){
+        const mid = Math.floor((left+right)/2)
 
+        if(nums[mid]===target){
             return mid
+        }else if(nums[mid]<target){
+            left = mid+1
+        }else{
+            right = mid;
         }
     }
-    return mid
+
+    return left
 }
