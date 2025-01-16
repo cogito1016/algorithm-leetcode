ae16e092d5f4fb4dd35b4c08f83b620d278b3eee
cogito1016 <cogito1016@naver.com>
2025-01-16 23:45:19 +0900

    refactor: 계단오르기, 1과2는 즉답, 점화식에맞게 2개의 변수만 활용

--- 0070-climbing-stairs/run.ts
+++ 0070-climbing-stairs/run.ts
@@ -1,16 +1,18 @@
 const n = 3
 console.log(solution(6))
 function solution(n:number): number{
-    const dp :number[] = Array(46).fill(0)
-    dp[1] = 1 // 1
-    dp[2] = 2 // 1 , 2
-    dp[3] = 3 // 1 1 1, 1 2, 2 1
-    dp[4] = 5 // 1 1 1 1, 1 1 2, 1 2 1, 2 1 1, 2 2
-    dp[5] = 8 // 1 1 1 1 1, 1 1 1 2, 1 1 2 1, 1 2 1 1, 2 1 1 1, 2 2 1, 2 1 2, 1 2 2,
+    if(n===1) return 1;
+    if(n===2) return 2;
 
-    for(let idx=6; idx<46; idx++){
-        dp[idx] = dp[idx-1] + dp[idx-2]
+    let first = 1 // 1
+    let second = 2 // 1 , 2
+    let now = 0;
+
+    for(let i=3; i<=n; i++){
+        now = first + second;
+        first = second;
+        second = now;
     }
 
-    return dp[n];
+    return now;
 }
