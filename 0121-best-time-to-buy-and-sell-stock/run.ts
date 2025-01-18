81ebc6f56dd7d02fbfc776a9e2418b8199a31f56
cogito1016 <cogito1016@naver.com>
2025-01-19 02:21:57 +0900

    feat: 쌀때사고 비쌀때판다

--- 0121-best-time-to-buy-and-sell-stock/run.ts
+++ 0121-best-time-to-buy-and-sell-stock/run.ts
@@ -1,16 +1,20 @@
-const prices:number[] = [7,1,5,3,6,4]
+const prices:number[] = [4,7,1,2,11]
 console.log(solution(prices))
 
 function solution(prices:number[]):number{
-    const LENGTH = prices.length;
-    let max = 0;
-    for(let i=0;i<LENGTH; i++){
-        for(let j=i+1; j<LENGTH; j++){
-            if(prices[j]-prices[i] > max){
-                max = prices[j]-prices[i]
-            }
-        }
+
+    let min = prices[0];
+    let maxProfit = 0;
+    for(let i=1; i<prices.length; i++){
+        const currentValue = prices[i];
+
+        if(min>currentValue)
+            min=currentValue
+
+        const profit = currentValue-min
+        if(currentValue-min > maxProfit)
+            maxProfit = profit
     }
 
-    return max
+    return maxProfit
 }
