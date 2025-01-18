74d67212925d45dee3b58489820c0d70b078a037
cogito1016 <cogito1016@naver.com>
2025-01-19 02:36:17 +0900

    feat: 최다등장 수 탐색'

---
+++ 0169-majority-element/run.ts
@@ -0,0 +1,31 @@
+const nums = [2,2,1,1,1,2,2]
+console.log(solution(nums))
+
+function solution(nums:number[]){
+    const LENGTH = nums.length;
+
+    if(LENGTH===1)
+        return nums[0]
+
+    const map = new Map<number,number>
+    let max = 0;
+    let majority = 0;
+
+    for(let i=0;i<LENGTH;i++){
+        const num = nums[i];
+        if(!map.has(num)){
+            map.set(num,1)
+            continue
+        }
+
+        const newValue = map.get(num) + 1
+        map.set(num,newValue)
+
+        if(newValue>max){
+            max=newValue
+            majority=num
+        }
+    }
+
+    return majority
+}
