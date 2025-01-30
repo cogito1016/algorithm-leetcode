811db16dec71f4cdd9abaa8dfeac4808c1772076
김재형 <cogito1016@naver.com>
2025-01-30 15:43:30 +0900

    check

---
+++ 0070-climbing-stairs/run.ts
@@ -0,0 +1,13 @@
+const n = 2
+
+
+//1 = 1
+//2 = 2
+//3 = 4
+//4 = 7
+// 1 1 1 1 , 1 3, 3 1, 1 2 1, 2 1 1, 1 1 2, 2 2,
+
+// 이건 DP일까?
+function solution(n:number):number {
+    const dic = [1,2,4,7]
+}
