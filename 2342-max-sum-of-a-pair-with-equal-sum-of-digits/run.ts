8c5229c7056a488f36e3ab9f9abb03ddd22f3b2f
김재형 <cogito1016@naver.com>
2025-02-12 19:54:03 +0900

    refactor: indexOf를 사용하여 index를 value로 저장하지말고, value를 map의 value로 지정하도록 개선하여 indexOf제거하여 속도개선, 통과

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/run.ts
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/run.ts
@@ -7,17 +7,17 @@ function maximumSum(nums: number[]): number {
     let max = -1;
     const mapOfsumOfNums:Map<number,number> = new Map<number,number>();
 
-    nums.map((num)=>num.toString().split("").reduce((sum,digit)=>sum+Number(digit),0))
-        .forEach((num, index)=> {
-                if (mapOfsumOfNums.has(num)) {
-                    const sum = nums[index] + nums[mapOfsumOfNums.get(num)]
-                    const maximumValue = Math.max(nums[index], nums[mapOfsumOfNums.get(num)])
-                    mapOfsumOfNums.set(num, nums.indexOf(maximumValue))
-                    max = Math.max(sum, max)
-                }else{
-                    mapOfsumOfNums.set(num,index)
-                }
-            }
-        )
+    nums.forEach((num,index)=> {
+        const sumOfPartOfNums = num.toString().split("").reduce((sum,digit)=>sum+Number(digit),0)
+
+        if (mapOfsumOfNums.has(sumOfPartOfNums)) {
+            const sum = nums[index] + mapOfsumOfNums.get(sumOfPartOfNums)
+            const maximumValue = Math.max(nums[index], mapOfsumOfNums.get(sumOfPartOfNums))
+            mapOfsumOfNums.set(sumOfPartOfNums, maximumValue)
+            max = Math.max(sum, max)
+        }else{
+            mapOfsumOfNums.set(sumOfPartOfNums,nums[index])
+        }
+    })
     return max;
 };
