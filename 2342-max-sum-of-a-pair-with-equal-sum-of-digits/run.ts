8735525b63f92e202bca1e4df2339f96cb1dd56a
김재형 <cogito1016@naver.com>
2025-02-12 19:30:47 +0900

    refactor: 자릿수의 합을 미리 계산하여 시간개선, 그러나 시간초과

--- 2342-max-sum-of-a-pair-with-equal-sum-of-digits/run.ts
+++ 2342-max-sum-of-a-pair-with-equal-sum-of-digits/run.ts
@@ -5,17 +5,15 @@ console.log(maximumSum(nums))
 
 function maximumSum(nums: number[]): number {
     let max = -1;
+    let sumOfNums = nums.map((num)=>num.toString().split("").reduce((sum,digit)=>sum+Number(digit),0))
 
     for(let i=0; i<nums.length; i++){
         const iNum = nums[i]
-        let sumPartOfI = iNum.toString().split("").reduce((sum,digit)=>sum+Number(digit),0)
-
 
         for(let j=i+1; j<nums.length; j++){
             const jNum = nums[j]
-            let sumPartOfJ = jNum.toString().split("").reduce((sum,digit)=>sum+Number(digit),0)
 
-            if(sumPartOfI===sumPartOfJ)
+            if(sumOfNums[i]===sumOfNums[j])
                 max = Math.max(max,iNum+jNum)
         }
     }
