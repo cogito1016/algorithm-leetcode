97f82c49003d02c183b8cb1186b58b6286a78860
김재형 <cogito1016@naver.com>
2025-02-14 14:08:49 +0900

    refactor: 부분적으로 DP사용, 시간초과

--- 1352-product-of-the-last-k-numbers/run.ts
+++ 1352-product-of-the-last-k-numbers/run.ts
@@ -1,15 +1,27 @@
 class ProductOfNumbers {
     arr:number[]
+    resultArr:number[]
+
     constructor() {
         this.arr = []
+        this.resultArr = []
     }
 
     add(num: number): void {
         this.arr = [num, ...this.arr]
+        this.resultArr = []
     }
 
     getProduct(k: number): number {
-        return this.arr.slice(0,k).reduce((num,digit)=> num*digit,1)
+        if(this.resultArr.length!==0 && this.resultArr.length >= k){
+            return this.resultArr[k-1]
+        }
+
+        this.arr.slice(0,k).forEach((num,idx)=>{
+            this.resultArr[idx] = num * (idx!==0?this.resultArr[idx-1] : 1)
+        })
+
+        return this.resultArr[k-1]
     }
 }
 
