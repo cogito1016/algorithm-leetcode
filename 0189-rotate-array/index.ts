19b883819f1285ca093983de9323bf338a794d04
김재형 <cogito1016@naver.com>
2025-03-25 20:28:40 +0900

    refactor: 797ms->1ms, 규칙이 있을것같은 문제는 코딩보단 규칙찾기가 우선인것같다.

--- 0189-rotate-array/index.ts
+++ 0189-rotate-array/index.ts
@@ -7,14 +7,34 @@ const k = 3
 
 rotate(nums,k)
 function rotate(nums: number[], k: number): void {
-    // const len = nums.length
+    
+    // while(k-->0){
+        const len = nums.length
+        k = k%len;
+        swap(nums,0,len-1)
+        swap(nums,0, k-1)
+        swap(nums,k,len-1)
 
-    while(k-->0){
-        // const lastValue = nums[len-1];
-        const lastValue = nums.pop()
-        if(lastValue===undefined)
-            break
-        nums.unshift(lastValue)
+        nums
         console.log(nums)
+    // }
+};
+
+function swap(nums:number[], start:number, end:number){
+    while(start<end){
+        [nums[start], nums[end]] = [nums[end], nums[start]]
+        start++
+        end--
     }
- };
+}
+
+// 1 2 3 4 5
+// 5 1 2 3 4
+// 4 5 1 2 3
+
+
+// 1 2 3 4 5
+// 5 4 3 2 1
+
+// 4 5 3 2 1
+// 4 5 1 2 3 
