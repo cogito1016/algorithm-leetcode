f21536d0d41cdc8743d4d52ac50576514cdc5911
김재형 <cogito1016@naver.com>
2025-03-25 19:35:08 +0900

    refactor: 한자리처리완료, 공간복잡도가 절약되는방법, 그러나 유지보수는 별로

--- 0035-remove-duplicates-from-sorted-array/index.ts
+++ 0035-remove-duplicates-from-sorted-array/index.ts
@@ -3,10 +3,20 @@ let nums:number[] = [0,0,1,1,1,2,2,3,3,4]
 console.log(removeDuplicates(nums))
 
 function removeDuplicates(nums: number[]): number {
-    const result = new Set<number>()
-    
-    nums.forEach((num)=>result.add(num))
-    nums = Array.from(result)
+    const len = nums.length
+    let targetIdx = 1;
+    let prevValue = nums[0];
 
-    return result.size;
+    for(let i=1; i<len; i++){
+        const currentValue = nums[i]
+
+        if(prevValue!==currentValue){
+            nums[targetIdx] = currentValue
+            prevValue=currentValue
+            targetIdx++;
+        }
+    }
+
+    console.log(nums)
+    return targetIdx;
 };
