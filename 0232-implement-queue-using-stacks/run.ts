77790f8dde3196a662c919255eaa20a0a18123ee
김재형 <cogito1016@naver.com>
2025-04-01 19:31:17 +0900

    feat: 큐만들기(리스트버전)

---
+++ 0232-implement-queue-using-stacks/run.ts
@@ -0,0 +1,33 @@
+class MyQueue {
+    queue:number[]
+
+    constructor() {
+        this.queue = []        
+    }
+
+    push(x: number): void {
+        this.queue.push(x)
+    }
+
+    pop(): number {
+        const result = this.queue.shift() ?? 0
+        return result
+    }
+
+    peek(): number {
+        return this.queue[0]
+    }
+
+    empty(): boolean {
+        return this.queue.length===0
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * var obj = new MyQueue()
+ * obj.push(x)
+ * var param_2 = obj.pop()
+ * var param_3 = obj.peek()
+ * var param_4 = obj.empty()
+ */
