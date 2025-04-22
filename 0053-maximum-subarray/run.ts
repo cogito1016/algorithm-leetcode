2794819e1dd3df68bf43b4d72bca96e3f02a85be
김재형 <cogito1016@naver.com>
2025-04-22 19:33:01 +0900

    refactor: 2ms 64MB 카데인알고리즘활용

--- 0053-maximum-subarray/run.ts
+++ 0053-maximum-subarray/run.ts
@@ -4,18 +4,12 @@ console.log(maxSubArray(nums))
 
 function maxSubArray(nums: number[]): number {
   const numsLen = nums.length;
-  let max = nums[0];
+  let max = -10001;
+  let sum = 0;
   for(let i=0; i<numsLen; i++){
-    const iNum = nums[i]
-    let sum = iNum;
-    if(sum>max)
-        max=sum
-
-    for(let j=i+1; j<numsLen; j++){
-        sum+=nums[j]
-        if(sum>max)
-            max=sum
-    }
+    sum = Math.max(sum+nums[i],nums[i])
+    if(sum > max)
+        max = sum
   }
   return max
 };
