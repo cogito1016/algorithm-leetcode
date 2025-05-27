60acb89cc729587039531606924b4187ec4be091
김재형 <cogito1016@naver.com>
2025-05-27 20:27:06 +0900

    feat: 0045 1ms 56MB jump game 2, greedy

--- 0045-jump-game-2/run.ts
+++ 0045-jump-game-2/run.ts
@@ -2,18 +2,23 @@ const nums = [1,2,3];
 console.log(jump(nums))
 
 function jump(nums: number[]): number {
-    let max = nums[0]
+    let destination = nums[0]
 
-    if(max===0 || nums.length===1)
+    if(destination===0 || nums.length===1)
         return 0
 
     let switchCount = 1;
-    for(let i=0; i<=max; i++){
+    let max = destination
+    for(let i=0; i<=destination; i++){
 
-        if(max>=nums.length-1)
+        if(destination>=nums.length-1)
             break
         if(i+nums[i] > max){
             max = i+nums[i]
+        }
+
+        if(i===destination){
+            destination=max;
             switchCount++
         }
     }
