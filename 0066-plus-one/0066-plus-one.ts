d4e2ade0366008bb8ab20b60944532c433544ceb
KimJaeHyeong <35924958+cogito1016@users.noreply.github.com>
2026-03-05 23:35:18 +0900

    Time: 0 ms (100%), Space: 53.4 MB (98.97%) - LeetHub

--- 0066-plus-one/0066-plus-one.ts
+++ 0066-plus-one/0066-plus-one.ts
@@ -1,12 +1,11 @@
 function plusOne(digits: number[]): number[] {
-    const lastIdx = digits.length-1;
-
-    for(let i = lastIdx; i>=0; i--){
+    for(let i=digits.length-1; i>=0; i--){
         if(digits[i]<9){
             digits[i]++
             return digits
         }
-        digits[i] = 0;
+
+        digits[i]=0;
     }
 
     return [1,...digits]
