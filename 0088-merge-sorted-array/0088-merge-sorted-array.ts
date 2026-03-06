e9b879124ff38d63ddc206d11995bbd2dc48cfb2
KimJaeHyeong <35924958+cogito1016@users.noreply.github.com>
2026-03-07 00:52:33 +0900

    Time: 0 ms (100%), Space: 55.7 MB (83.04%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.ts
+++ 0088-merge-sorted-array/0088-merge-sorted-array.ts
@@ -10,15 +10,18 @@
 //     nums1.splice(0,nums1.length,...(nums1ExceptZero.concat(nums2ExceptZero)).sort((a,b)=>a-b))
 // };
 
-
-
 function merge(nums1: number[], m: number, nums2: number[], n: number): void {
-    let i=m-1,j=n-1,k=nums1.length-1
+    //splice는 내부적으로 요소를 옮기므로 느리다 O(n)
+    //concat은 배열을 생성하여 두 배열을 순회하므로 느리다 O(n+m)
+    //sort는 대체로 Timesort를 이용한다 O(nlogn)   
+    //문제를 이해하고 인덱스로 해결하는 방식을 생각해야한다.
+
+    let i=m-1,j=n-1,k=m+n-1
+
     while(k>=0){
-
         if(j<0)
-            break;
-
+            break
+        
         if(nums1[i]>nums2[j]){
             nums1[k]=nums1[i]
             i--
@@ -26,7 +29,6 @@ function merge(nums1: number[], m: number, nums2: number[], n: number): void {
             nums1[k]=nums2[j]
             j--
         }
-
         k--
     }
 };
